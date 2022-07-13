cc0949d51073964efa6a261625c778e14a1b0793
umang_patel <umang.patel@alshaya.com>
2022-07-13 20:28:44 +0530

    longestSubStringHashMap done

---
+++ longestSubStringHashMap.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+class longestSubStringHashMap {
+    public static int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int slow = 0;
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                slow = Math.max(slow, map.get(c));
+            }
+            max = Math.max(max, i - slow + 1);
+            map.put(c, i + 1);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+}
